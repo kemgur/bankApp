310334a56d2f51780d90a053678b19227998b387
kemalgurler <kemgur52@gmail.com>
2021-07-19 16:52:11 -0400

    Jul19

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,52 +1,29 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import {Link} from 'react-router-dom';
-import logo from "../"
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
 
-const Header=()=> {
-  const classes = useStyles();
-  
-    return (
-        <div>
-           <AppBar position="static">
-  <Toolbar variant="dense">
-   <Link>
-      <img src={logo} alt="logo"/>
-   </Link>
-    <IconButton 
-    edge="start" 
-    className={classes.menuButton} 
-    color="inherit" 
-    aria-label="menu">
+const Header = () => {
+  return (
+    <div>
+    <Container fluid>
+      <Nav variant="pills" defaultActiveKey="/home">
+        <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+      Disabled
+       </Nav.Link>
+      </Nav.Item>
+     </Nav>
+        
+       
 
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" className={classes.title}>
-      News
-    </Typography>
-    <Button color="inherit">Login</Button>
-  </Toolbar>
-</AppBar>
-        </div>
-    )
+    </Container>
+    </div>
+  )
 }
-
 export default Header
